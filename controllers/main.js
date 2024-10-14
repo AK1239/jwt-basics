497ebb6b72a85775dc160f6c374985fd64290932
AK1239 <akilkhatri235@gmail.com>
2024-10-14 17:43:22 +0300

    login controller

--- controllers/main.js
+++ controllers/main.js
@@ -1,5 +1,18 @@
+const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
+
 const login = async (req, res) => {
-  res.send("login");
+  const { username, password } = req.body;
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide an email and password", 400);
+  }
+
+  const id = new Date().getDate();
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).json({ msg: "user created", token });
 };
 
 const dashboard = async (req, res) => {
